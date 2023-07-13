2439f5efc0c88ed5aed9a657ec21685beb732796
Ritesh 리떼쉬 <rit.kr.7@gmail.com>
2023-07-13 13:13:45 +0530

    i18Demo page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Suspense, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const locales = {
+  en: { title: 'English' },
+  kr: { title: 'Korean' },
+  hi: { title: 'Hindi' }
+};
+
 
 function App() {
+  const [message, setMessage] = useState(0);
+  const { t, i18n } = useTranslation();
+  console.log(message);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {Object.keys(locales).map((locale) =>
+          <li key={locale}><button
+            style={{ fontWeight: i18n.resolvedLanguage === locale ? 'bold' : '' }}
+            type='submit' onClick={() => i18n.changeLanguage(locale)} >{locale}
+          </button></li>
+        )}
+      </ul>
+      <h1>{t('main.header')}</h1>
+      <p>This is demo test for i18next</p>
+      <button onClick={() => setMessage(message + 1)}>+1 message</button>
+      <p>{t('main.new_message', { count: message })}</p>
+      <p>{t('main.current_date', { date: new Date() })}</p>
+
+    </>
   );
 }
 
-export default App;
+export default function WrapperApp() {
+  return (
+    <Suspense fallback="...loading">
+      <App />
+    </Suspense>
+  )
+};
